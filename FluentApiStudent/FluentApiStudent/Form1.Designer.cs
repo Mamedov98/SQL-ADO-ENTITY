3b523b771d0064b9c23a6d01a875ae4fdfd05bd9
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-02-21 16:52:54 +0400

    Add files via upload

---
+++ FluentApiStudent/FluentApiStudent/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FluentApiStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.Label();
+            this.Surname = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Group = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 192);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 258);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(200, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(359, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(534, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 5;
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(71, 80);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(49, 20);
+            this.Name.TabIndex = 6;
+            this.Name.Text = "Name";
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Location = new System.Drawing.Point(227, 80);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(67, 20);
+            this.Surname.TabIndex = 7;
+            this.Surname.Text = "Surname";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Birthday Date";
+            // 
+            // Group
+            // 
+            this.Group.AutoSize = true;
+            this.Group.Location = new System.Drawing.Point(572, 80);
+            this.Group.Name = "Group";
+            this.Group.Size = new System.Drawing.Size(50, 20);
+            this.Group.TabIndex = 9;
+            this.Group.Text = "Group";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(596, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 54);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Delete Student";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 54);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Edit Student";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Group);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            //this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label Name;
+        private Label Surname;
+        private Label label1;
+        private Label Group;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ FluentApiStudent/FluentApiStudent/Model/Student.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace FluentApiStudent.Model
+{
+  /* public class Student
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Group { get; set; }
+
+    }*/
+  
+    public class StudentContext : DbContext
+    {
+        
+        public DbSet<Student> Students { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentFluentApi;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+               
+                entity.Property(x => x.Name)
+                .IsRequired();
+                entity.Property(entity => entity.Surname)
+                .IsRequired();
+                entity.Property(entity => entity.DateOfBirth)
+                .IsRequired();
+                entity.Property(entity => entity.Group);
+            });
+        }
+    }
+   public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string DateOfBirth { get; set;}
+        public string Group { get; set; }
+    }
+}
+
